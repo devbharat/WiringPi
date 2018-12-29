8ae966df7b040751e569e8649eae1a493495cdfa
devbharat <bharat.tak8@gmail.com>
2018-12-29 14:41:18 +0100

    Move CMakelist file to .cmake file that is included in to level cmakelist file

--- wiringPi.cmake
+++ wiringPi.cmake
@@ -4,17 +4,22 @@ cmake_minimum_required(VERSION 2.8)
 
 set(CMAKE_WIRINGPI_DIR "${CMAKE_SOURCE_DIR}/wiringPi/wiringPi")
 set(CMAKE_WIRINGPI_LIB "${CMAKE_SOURCE_DIR}/wiringPi/wiringPi/libwiringPi.a")
+
 IF(CMAKE_CROSSCOMPILING)
 add_custom_command(
    OUTPUT ${CMAKE_WIRINGPI_LIB}
+   COMMAND make clean
    COMMAND make CC=${CMAKE_C_COMPILER} static
    WORKING_DIRECTORY ${CMAKE_WIRINGPI_DIR}
+   BYPRODUCTS ${TMP_WIRINGPI}
    )
 ELSE(CMAKE_CROSSCOMPILING)
 add_custom_command(
    OUTPUT ${CMAKE_WIRINGPI_LIB}
+   COMMAND make clean
    COMMAND make static
    WORKING_DIRECTORY ${CMAKE_WIRINGPI_DIR}
+   BYPRODUCTS ${TMP_WIRINGPI}
    )
 ENDIF(CMAKE_CROSSCOMPILING)
 add_custom_target(
@@ -22,6 +27,6 @@ add_custom_target(
    DEPENDS ${CMAKE_WIRINGPI_LIB}
    )
 
-add_library(wiringPi STATIC)
-target_include_directories(wiringPi PUBLIC ${CMAKE_WIRINGPI_DIR})
+add_library(wiringPi STATIC IMPORTED)
+SET_PROPERTY(TARGET wiringPi PROPERTY IMPORTED_LOCATION ${CMAKE_WIRINGPI_LIB})
 add_dependencies(wiringPi wiringpi)
